bed6f7ed7ebe2d43ecd0c880271f3bb373c30deb
JJ <614637405@qq.com>
2021-07-13 17:26:57 +0800

    Mydemo

--- src/main/java/com/adamhuang/mydemo/controller/HelloWorldController.java
+++ src/main/java/com/adamhuang/mydemo/controller/HelloWorldController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String Hello(){
-        return "Hello World";
+        return "Hello World！爷是爱因不喝咖啡";
     }
 }
